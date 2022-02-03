71392bd298e903a072c6b0eeccc69474c030a388
sakaiken18 <sakaiken6@gmail.com>
2022-02-03 20:56:33 +0900

    moviesテーブルのカラムを修正

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_02_132253) do
+ActiveRecord::Schema.define(version: 2022_02_03_115344) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -18,9 +18,9 @@ ActiveRecord::Schema.define(version: 2022_02_02_132253) do
   create_table "movies", force: :cascade do |t|
     t.integer "genre", default: 0, null: false
     t.string "title", null: false
-    t.text "content", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "url", null: false
   end
 
   create_table "texts", force: :cascade do |t|

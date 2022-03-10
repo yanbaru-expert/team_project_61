4e3694f9f51428e27baffefbe07e3a2b559d40d8
daichi kouno <15214kouno@yuai.ac.jp>
2022-03-10 11:05:00 +0900

    Embedsアスペクト比の追加

--- app/helpers/movies_helper.rb
+++ app/helpers/movies_helper.rb
@@ -1,8 +1,8 @@
 module MoviesHelper
   def embed_youtube(url)
     tag.iframe(
-      width: 360,
-      height: 315,
+      width: 200,
+      height: 350,
       src: url,
       frameborder: 0,
       allow: "accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture",

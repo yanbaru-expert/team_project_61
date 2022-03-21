5cc1bda824fbfbbe3b1192c2821801a567c0a742
sakaiken18 <sakaiken6@gmail.com>
2022-03-21 16:00:07 +0900

    管理画面の各ページのカラム・フォームを修正　Activeadminで使用しているcssとjsをvedorディレクトリに移動

--- app/admin/movies.rb
+++ app/admin/movies.rb
@@ -1,18 +1,38 @@
 ActiveAdmin.register Movie do
+  permit_params :genre, :title, :content
 
-  # See permitted parameters documentation:
-  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
-  #
-  # Uncomment all parameters which should be permitted for assignment
-  #
-  # permit_params :genre, :title, :url
-  #
-  # or
-  #
-  # permit_params do
-  #   permitted = [:genre, :title, :url]
-  #   permitted << :other if params[:action] == 'create' && current_user.admin?
-  #   permitted
-  # end
-  
+  index do
+    selectable_column
+    id_column
+    column :genre, &:genre_i18n
+    column :title
+    actions
+  end
+  show do
+    attributes_table do
+      row :id
+      row :genre, :text, &:genre_i18n
+      row :title
+      row :content
+      row :created_at
+      row :updated_at
+    end
+    active_admin_comments
+  end
+
+  form do |f|
+    f.semantic_errors
+    f.inputs do
+      input :genre, as: :select, collection: Movie.genres_i18n.invert
+      input :title
+      input :content
+    end
+    f.actions
+  end
+
+  filter :genre, as: :select, collection: Movie.genres_i18n.invert.transform_values { |v| Movie.genres[v] }
+  filter :title
+  filter :content
+  filter :created_at
+  filter :updated_at
 end

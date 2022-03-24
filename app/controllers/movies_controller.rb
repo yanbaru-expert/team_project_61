a7e3ff8416336f496f7e8ee49addf3c8f56bd2e6
daichi kouno <15214kouno@yuai.ac.jp>
2022-03-25 00:54:04 +0900

    差分解消

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,10 +1,9 @@
 class MoviesController < ApplicationController
   def index
     if params[:genre] == "php"
-      @movies = Movie.where(genre: PHP_GENRE_LIST) 
+      @movies = Movie.where(genre: PHP_GENRE_LIST)
     else
       @movies = Movie.where(genre: RAILS_GENRE_LIST)
     end
-    
   end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -9,12 +9,4 @@ module ApplicationHelper
       "mw-xl"
     end
   end
-
-  def full_title
-    if params[:genre] == "php"
-      
-    else
-      "#{page_title} - #{BASE_TITLE}"
-    end
-  end
 end

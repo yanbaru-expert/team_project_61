7709ca2834da117404ec60894717c669f2c85996
sakaiken18 <sakaiken6@gmail.com>
2022-03-31 23:34:15 +0900

    テキスト教材一覧ページに読破済み機能を実装

--- app/controllers/texts_controller.rb
+++ app/controllers/texts_controller.rb
@@ -1,6 +1,6 @@
 class TextsController < ApplicationController
   def index
-    @texts = Text.where(genre: RAILS_GENRE_LIST)
+    @texts = Text.where(genre: RAILS_GENRE_LIST).includes(:read_progresses)
   end
 
   def show

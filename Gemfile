02923795479c6f7508a210e25269b2a11fd6de8c
daichi kouno <15214kouno@yuai.ac.jp>
2022-04-05 18:29:47 +0900

    ページネーションの実装

--- Gemfile
+++ Gemfile
@@ -36,3 +36,4 @@ end
 gem "tzinfo-data", platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 
 gem "enum_help"
+gem 'kaminari'
--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,9 +1,11 @@
 class MoviesController < ApplicationController
+  PER_PAGE = 12
   def index
     @movies = if params[:genre] == "php"
                 Movie.where(genre: PHP_GENRE_LIST)
               else
                 Movie.where(genre: RAILS_GENRE_LIST)
               end
+              @movies = Movie.page(params[:page]).per(PER_PAGE)
   end
 end

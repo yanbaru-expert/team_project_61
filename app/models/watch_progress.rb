7bef814389e03abf42b334ed775ec3b1761a75c0
mczloves <mczloves1@yahoo.co.jp>
2022-04-11 17:23:01 +0900

    読破済みを非同期で実装

--- app/models/watch_progress.rb
+++ app/models/watch_progress.rb
@@ -1,4 +1,8 @@
 class WatchProgress < ApplicationRecord
   belongs_to :user
   belongs_to :movie
+  validates :user_id, uniqueness: {
+                      scope: :movie_id,
+                      message: "は同じテキスト教材を２回以上読破済みにはできません",
+                      }
 end
